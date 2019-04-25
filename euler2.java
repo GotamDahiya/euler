65112471a77ccb8857572e8e42c94f8e3ee8da9a
Gotam Dahiya <44541256+GotamDahiya@users.noreply.github.com>
2019-04-25 23:44:26 +0530

    Euler #2

---
+++ euler2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class euler2
+{
+	public static void main(String[] args) 
+	{
+		Scanner read=new Scanner(System.in);
+		int t=read.nextInt();
+		for(int i=0;i<t;i++)
+		{
+			long n=read.nextLong();
+			for(int j=0;j<n;j++)
+			{
+				
+			}
+		}
+	}
+}
