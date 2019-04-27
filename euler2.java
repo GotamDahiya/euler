db76c43dbf10b00b9f96bf7d61346a91b9ecbbe0
Prateek <f20160602@hyderabad.bits-pilani.ac.in>
2019-04-27 17:29:42 +0530

    Euler #2

--- euler2.java
+++ euler2.java
@@ -10,10 +10,18 @@ public class euler2
 		for(int i=0;i<t;i++)
 		{
 			long n=read.nextLong();
-			for(int j=0;j<n;j++)
+			int f0=2;
+			int f1=8;
+			int f2=0;
+			int sum=f0;
+			while(f1<n)
 			{
-				
+				sum+=f1;
+				f2=4*f1+f0;
+				f0=f1;
+				f1=f2;
 			}
+			System.out.println(sum);
 		}
 	}
 }
