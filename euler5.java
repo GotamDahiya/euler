433892b71cc482897663d21ba00e8fb0304a13cc
GotamDahiya <apollo2562000@gmail.com>
2019-04-29 00:06:12 +0530

    Euler #5

---
+++ euler5.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+public class euler5
+{
+	public static void main(String[] args) 
+	{
+		Scanner read=new Scanner(System.in);
+		int t=read.nextInt();
+		while(t-->0)
+		{
+			int n=read.nextInt();
+			for(int i=2;i<=n;i++)
+			{
+				for(int j=i;j<=n;j++)
+				{
+					if()
+				}
+			}
+		}	
+	}
+}
